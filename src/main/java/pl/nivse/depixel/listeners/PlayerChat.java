fe6b395e4620a8d4f10df53811ad70ffc0539afe
Mateusz <115919329+nh3xd@users.noreply.github.com>
2024-03-16 21:13:40 +0100

    Update PlayerChat.java

--- src/main/java/pl/nivse/depixel/listeners/PlayerChat.java
+++ src/main/java/pl/nivse/depixel/listeners/PlayerChat.java
@@ -70,9 +70,7 @@ public class PlayerChat implements Listener {
             Component delimiter = parseDisplayName(player, Depixel.getPlugin().getConfig().getString("chat.delimiter"));
             Component message = parseMessage(player, plainTextComponentSerializer.serialize(originalMessage));
 
-            Component renderedMessage = Component.empty();
-
-            renderedMessage = renderedMessage.append(displayName).append(Component.text(" ")).append(delimiter).append(Component.text(" ")).append(message);
+            Component renderedMessage = Component.empty().append(displayName).append(Component.text(" ")).append(delimiter).append(Component.text(" ")).append(message);
             return renderedMessage;
         }));
     }
