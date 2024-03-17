741938482e7f816429f1507ee4e50b6e467e10ba
nivse <sraken_pierdaken@wp.pl>
2024-03-17 22:10:46 +0100

    add javadocs

--- src/main/java/pl/nivse/depixel/objects/DepixelPlayer.java
+++ src/main/java/pl/nivse/depixel/objects/DepixelPlayer.java
@@ -17,14 +17,31 @@ public class DepixelPlayer {
     public Player getPlayer() {
         return player;
     }
+
+    /**
+     * Retrieves the last player who sent a message to this player.
+     *
+     * @return The last player who messaged this player.
+     */
     public DepixelPlayer getLastMessenger(){
         return lastMessenger;
     }
 
+
+    /**
+     * Sets the last player who sent a message to this player.
+     *
+     * @param lastMessenger The last player who messaged this player.
+     */
     public void setLastMessenger(DepixelPlayer lastMessenger) {
         this.lastMessenger = lastMessenger;
     }
 
+    /**
+     * Retrieves the formatted display name of the player with MiniMessage compatible format.
+     *
+     * @return The display name of the player for chat messages.
+     */
     public String getDisplayName() {
         String displayName = Depixel.getPlugin().getConfig().getString("chat.displayName")
                 .replace("{player}", player.getName());
--- src/main/java/pl/nivse/depixel/services/DepixelPlayerService.java
+++ src/main/java/pl/nivse/depixel/services/DepixelPlayerService.java
@@ -9,15 +9,32 @@ import java.util.Map;
 public class DepixelPlayerService {
     private final Map<Player, DepixelPlayer> players = new HashMap<>();
 
-    public void addPlayer(Player player){
+    /**
+     * Adds a player to the player map.
+     *
+     * @param player The player to be added.
+     */
+    public void addPlayer(Player player) {
         this.players.put(player, new DepixelPlayer(player));
     }
 
-    public DepixelPlayer getPlayer(Player player){
+    /**
+     * Retrieves the DepixelPlayer instance associated with player.
+     *
+     * @param player DepixelPlayer instance of given player to retrieve
+     * @return The DepixelPlayer instance of the given player.
+     */
+    public DepixelPlayer getPlayer(Player player) {
         return this.players.get(player);
     }
 
+    /**
+     * Removes the player from the player map.
+     *
+     * @param player The player to be removed.
+     */
     public void removePlayer(Player player) {
         this.players.remove(player);
     }
+
 }
